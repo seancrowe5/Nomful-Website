c3e9e240cde8a1647c1f926788b55c9174e990da
Thomas Gorczynski <me@thomasgorczynski.com>
2015-06-23 15:58:08 -0500

    again

--- join/discount-verify.php
+++ join/discount-verify.php
@@ -9,6 +9,15 @@
 //dev
 ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5');
 
+//test
+$stripe = array(
+  "secret_key"      => "sk_test_uMTJhaR5rAwMtnsLLKH9wTBx",
+  "publishable_key" => "pk_test_JQ0bC9TcrUXB06bKoMjYjw3l"
+);
+
+
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
+
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       try {
