ada568090e9913f8254b4e139359e7ab6221b197
seancrowe5 <sean.crowe@me.com>
2015-06-23 17:07:32 -0500

    payment meta

--- join/payment.php
+++ join/payment.php
@@ -21,7 +21,7 @@ require_once('../config.php');
   <meta property="og:title" content="" /> 
   <meta property="og:image" content="" /> 
   <meta property="og:description" content="" /> 
-  <!-- <meta property="og:url" content="https://nomful.com/development"> -->
+  <meta property="og:url" content="https://nomful.com/development"> 
   <meta name="author" content="nomful" /> 
   <meta name="robots" content="index, follow, noydir" /> 
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
