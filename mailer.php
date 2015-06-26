b117cc1d958661fabf9a64938ad080df3cfd9b3a
seancrowe5 <sean.crowe@me.com>
2015-06-26 13:45:31 -0500

    madrid for join testing

--- mailer.php
+++ mailer.php
@@ -29,16 +29,103 @@
         // Build the email headers.
         $email_headers = "From: <$email>";
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You!";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong.";
-        }
+    
+        try {
+    $mandrill = new Mandrill('QcW7X7TjFMCbR6eC9lqZWQ');
+    $message = array(
+        'html' => '<p>Example HTML content</p>',
+        'text' => 'Example text content',
+        'subject' => 'example subject',
+        'from_email' => 'sean@nomful.com',
+        'from_name' => 'Sean',
+        'to' => array(
+            array(
+                'email' => $recipient,
+                'name' => 'Recipient Name',
+                'type' => 'to'
+            )
+        ),
+        'headers' => array('Reply-To' => 'sean@nomful.com'),
+        'important' => false,
+        'track_opens' => null,
+        'track_clicks' => null,
+        'auto_text' => null,
+        'auto_html' => null,
+        'inline_css' => null,
+        'url_strip_qs' => null,
+        'preserve_recipients' => null,
+        'view_content_link' => null,
+        'bcc_address' => null,
+        'tracking_domain' => null,
+        'signing_domain' => null,
+        'return_path_domain' => null,
+        'merge' => true,
+        'merge_language' => 'mailchimp',
+        'global_merge_vars' => array(
+            array(
+                'name' => 'merge1',
+                'content' => 'merge1 content'
+            )
+        ),
+        'merge_vars' => array(
+            array(
+                'rcpt' => $recipient,
+                'vars' => array(
+                    array(
+                        'name' => 'merge2',
+                        'content' => 'merge2 content'
+                    )
+                )
+            )
+        ),
+        'tags' => array('password-resets'),
+        'subaccount' => 'customer-123',
+        'google_analytics_domains' => array('example.com'),
+        'google_analytics_campaign' => 'sean@nomful.com',
+        'metadata' => array('website' => 'www.nomful.com'),
+        'recipient_metadata' => array(
+            array(
+                'rcpt' => $recipient,
+                'values' => array('user_id' => 123456)
+            )
+        ),
+        'attachments' => array(
+            array(
+                'type' => 'text/plain',
+                'name' => 'myfile.txt',
+                'content' => 'ZXhhbXBsZSBmaWxl'
+            )
+        ),
+        'images' => array(
+            array(
+                'type' => 'image/png',
+                'name' => 'IMAGECID',
+                'content' => 'ZXhhbXBsZSBmaWxl'
+            )
+        )
+    );
+    $async = false;
+    $ip_pool = 'Main Pool';
+    $send_at = 'example send_at';
+    $result = $mandrill->messages->send($message, $async, $ip_pool, $send_at);
+    print_r($result);
+} catch(Mandrill_Error $e) {
+    // Mandrill errors are thrown as exceptions
+    echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
+    // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    throw $e;
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
