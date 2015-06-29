336b5aee38b9a74b34478f3ddcd4f7c82ed0e73a
seancrowe5 <sean.crowe@me.com>
2015-06-29 11:17:38 -0500

    join

--- js/apps.js
+++ js/apps.js
@@ -55,7 +55,7 @@ $(function() {
 		})
 		.success(function(response) {
 			// Make sure that the formMessages div has the 'success' class.
-			window.location = "join/payment.php";
+			window.location = "/payment.php";
 		})
 		.fail(function(data) {
 			// Make sure that the formMessages div has the 'error' class.
