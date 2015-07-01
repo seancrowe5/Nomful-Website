81a0507cd3a1b115ad28afe8e681a0f24dd90c8b
seancrowe5 <sean.crowe@me.com>
2015-07-01 17:30:57 -0500

    nothing

--- join/discount-verify.php
+++ join/discount-verify.php
@@ -1,13 +1,5 @@
 <?php
-require_once('vendor/autoload.php');
 
-$stripe = array(
-  "secret_key"      => "sk_test_uMTJhaR5rAwMtnsLLKH9wTBx",
-  "publishable_key" => "pk_test_JQ0bC9TcrUXB06bKoMjYjw3l"
-);
-
-
-\Stripe\Stripe::setApiKey($stripe['secret_key']);
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
