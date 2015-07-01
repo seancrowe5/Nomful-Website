83bd1c7fc893015c59e4b92cdac28aa81e2423e8
seancrowe5 <sean.crowe@me.com>
2015-07-01 17:29:00 -0500

    hard code

--- join/discount-verify.php
+++ join/discount-verify.php
@@ -1,14 +1,13 @@
 <?php
-require_once('./vendor/autoload.php');
-    use Parse\ParseClient;
-    use Parse\ParseQuery;
-    use Parse\ParseObject;
-require_once('./config.php');
+require_once('vendor/autoload.php');
 
-//live
-//ParseClient::initialize('KjqhJkgvtVSsPA9SVHxq1Euad73fWhLWfVS4LNxO', '9V1I071QAS4aveQRkxabmIY2wzisgnGQ1UH4SVG0', 'j3W9jcVXVgWQDfamFbm7UodDiYOSPjinnEAR7EdS');
-//dev
-ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5');
+$stripe = array(
+  "secret_key"      => "sk_test_uMTJhaR5rAwMtnsLLKH9wTBx",
+  "publishable_key" => "pk_test_JQ0bC9TcrUXB06bKoMjYjw3l"
+);
+
+
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
