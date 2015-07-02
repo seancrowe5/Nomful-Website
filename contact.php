990ed65925f54bfd43e548c6cb6d8ce3f4384083
Thomas Gorczynski <me@thomasgorczynski.com>
2015-07-02 16:50:09 -0500

    adding a contact page

---
+++ contact.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8" /> 
+  <title>nomful | be nomful about food</title>
+  <meta name="keywords" content=""/>
+  <meta name="description" content="nomful | be nomful about food" />
+  <meta property="og:type" content="website" /> 
+  <meta property="og:title" content="" /> 
+  <meta property="og:image" content="" /> 
+  <meta property="og:description" content="" /> 
+  <!-- <meta property="og:url" content="https://nomful.com"> -->
+  <meta name="author" content="nomful" /> 
+  <meta name="robots" content="index, follow, noydir" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="google-site-verification" content="s-PDv36DeQ8GbPWkvISgveTSpQBsb7BQSMJTfiK_7Ps" />
+  <link rel="icon" 
+    type="image/png" 
+    href="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <script src="//use.typekit.net/zcg5xnb.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="/../css/normalize.css">
+  <link rel="stylesheet" href="/../css/skeleton.css">
+  <link rel="stylesheet" href="/../css/custom.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="/../images/favicon.png">
+
+  <!-- SCRIPTS
+  ================================================== -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+
+  <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+<body>  
+  <header> 
+    <div class="nav">
+      <img class="reg-logo" src="/../img/nomful.png"/>
+    </div>
+  </header><!-- /header -->
+  <section class="top-masthead">
+    <div class="masthead-content">
+      <h1>Contact us</h1>
+      <p>Have a question? Want to learn more about us? We'd love to hear from you!</p>
+    </div>
+  </section>
+  
+  <section class="masthead masthead-2">
+    <div class="masthead-content underlay-light">
+      <h1>Not a Class</h1>
+      <p>You get to know your coach, set goals together, and work through obstacles to get to where you want/need to be. Your Nomful coach is always there for you to keep you on track.</p>
+    </div>
+  </section>
+  
+  <section class="masthead masthead-3">
+    <div class="masthead-content underlay-light">
+        <h1>Nutrition support built around you</h1>
+        <p>Nomful makes eating healthy easy. Team up with a dedicated nutrition coach to get the personalized support you need. Transform your health.</p>
+    </div>
+  </section>
+
+<?php include 'footer.php';?>
