85f78cd2c4937f6530f949fab7f74c9a0037708e
Thomas Gorczynski <me@thomasgorczynski.com>
2015-07-15 10:47:21 -0500

    whatss

--- join/app/app-cookie.php
+++ join/app/app-cookie.php
@@ -27,6 +27,24 @@ ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJy
         
         //return the frist user with that number
         $user = $query->first();
+
+        try {
+            //parse code
+            //query users with that phone number
+            $query = ParseUser::query();
+            $query->equalTo("phoneNumber", $_POST["cell-phone"]); 
+            //return the frist user with that number
+            $user = $query->first(); 
+        } catch (ParseException $e){
+            
+            //object not found
+            //don't redirect to the signup flow
+            show message that phone number is not registered
+            http_response_code(500);
+            echo "Oops!";
+            echo $e->getMessage();
+            exit;
+        }
             
         //set email and firstname for the payment page
         $email = $user->get("email");
@@ -64,7 +82,7 @@ ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJy
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong.";
+            echo "Oops! Something went wrong. Problem.";
         }
 
     } else {
