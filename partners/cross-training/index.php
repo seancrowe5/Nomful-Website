57d1a7be51309eafd5afaeb6bae04ef377209efd
seancrowe5 <sean.crowe@me.com>
2015-07-27 11:40:32 -0400

    added cross training partner page

---
+++ partners/cross-training/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8" /> 
+  <title>nomful | be nomful about food</title>
+  <meta name="keywords" content=""/>
+  <meta name="description" content="nomful | be nomful about food" />
+  <meta property="og:type" content="website" /> 
+  <meta property="og:title" content="" /> 
+  <meta property="og:image" content="" /> 
+  <meta property="og:description" content="" /> 
+  <meta property="og:url" content="https://nomful.com">
+  <meta name="author" content="nomful" /> 
+  <meta name="robots" content="index, follow, noydir" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="google-site-verification" content="s-PDv36DeQ8GbPWkvISgveTSpQBsb7BQSMJTfiK_7Ps" />
+  <link rel="icon" 
+    type="image/png" 
+    href="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <script src="//use.typekit.net/zcg5xnb.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="/../css/normalize.css">
+  <link rel="stylesheet" href="/../css/skeleton.css">
+  <link rel="stylesheet" href="/../css/custom.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="/../images/favicon.png">
+
+  <!-- SCRIPTS
+  ================================================== -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
+
+  <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+<body>
+  
+  <div class="container mbudges">
+    <div class="tables center">
+      <img class="partnerlogo cell-middle" src="fitwithfr.png"/>
+      <img class="partnerlogo cell-middle" src="/../img/nomful.png"/>
+    </div>
+    <div class="top-masthead-join mbudge">
+      <p>If you have any additional questions or need help, email <a href="mailto:thomas@nomful.com">thomas@nomful.com</a> or call 847-226-1310.</p>
+    </div>
+  </div>
+  
+    
+  <div class="form-container">
+    <div class="row">
+      <div class="offset-by-three six columns center">
+        <a class="button" href="trainer-register.php">Trainer Registration</a>
+      </div>
+    </div>
+  </div>
+  
+  <style>
+    html { 
+    background: url(http://nomful.com/img/strawberry.jpg) no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  </style>
+  
+  <!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
