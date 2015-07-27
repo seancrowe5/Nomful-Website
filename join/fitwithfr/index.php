d534982b7d16e693845aa48fb23f22c8e1801d36
seancrowe5 <sean.crowe@me.com>
2015-07-27 13:48:43 -0400

    99 two weeks free

--- join/fitwithfr/index.php
+++ join/fitwithfr/index.php
@@ -4,7 +4,7 @@
   $partner_info = array(
     'plan_base' => 'focusedresults', //THIS HAS TO MATCH THE ID IN STRIPE
     'club' => 'Focused Results', // this is the NAME of the affiliate partner
-    'basic_price' => '99 - 2 Weeks Free', // this is the PRICE of the BASIC plan for this group code
+    'basic_price' => '99 2 Weeks Free', // this is the PRICE of the BASIC plan for this group code
     'premium_price' => '249', // this is the PRICE of the PREMIUM plan for this group code
     'gymID' => 'zQKLlKiYTP' //gym id dbXma3eUtI V0qgIi6L53
   );
