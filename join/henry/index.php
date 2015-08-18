bac3e6cb89ad92b8281ab12bcabbde1cf49675bd
seancrowe5 <sean.crowe@me.com>
2015-08-18 15:20:02 -0500

    henry

--- join/henry/index.php
+++ join/henry/index.php
@@ -1,3 +1,26 @@
+<?php 
+    //this is the co-braded page for Nomful + MAC
+
+  $partner_info = array(
+    'plan_base' => 'Henry', //THIS HAS TO MATCH THE ID IN STRIPE
+    'club' => 'Henry', // this is the NAME of the affiliate partner
+    'basic_price' => '99', // this is the PRICE of the BASIC plan for this group code
+    'premium_price' => '249', // this is the PRICE of the PREMIUM plan for this group code
+    'gymID' => 'ZeXbgXbcfy' //gym id for henry
+  );
+
+  $c = json_encode($partner_info);
+  setcookie(
+    'partner_info',				// Name of the cookie, required
+    $c,					        // The value of the cookie
+    time()+60*60*24,			// Expiration time, set for a week in the future
+    '/',						// Folder path the cookie will be available for
+    'nomful.com'		        // Domain to which the cookie will be bound
+  );
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,11 +77,11 @@
   
   <div class="container mbudges">
     <div class="tables center">
-      <img class="partnerlogo cell-middle" src="/../img/thedaileymethod.png"/>
+      <img class="partnerlogo cell-middle" src="/../img/ctplayground.png"/>
       <img class="partnerlogo cell-middle" src="/../img/nomful.png"/>
     </div>
-    <div class="top-masthead-join mbudge">
-      <p>If you have any additional questions or need help, email <a href="mailto:thomas@nomful.com">thomas@nomful.com</a> or call 847-226-1310.</p>
+    <div class="top-masthead-join mbudges">
+      <p>Nomful is proud to bring you with personalized nutrition support from the experts, right on your phone.<br>If you have any additional questions or need help, email <a href="mailto:thomas@nomful.com">support@nomful.com</a> or call us anytime at (562) 666 - 3858.</p>
     </div>
   </div>
   
@@ -66,7 +89,7 @@
   <div class="form-container">
     <div class="row">
       <div class="offset-by-three six columns center">
-        <a class="button" href="trainer-register.php">Trainer Registration</a>
+        <a class="button" href="../">Member Sign-up</a>
       </div>
     </div>
   </div>
