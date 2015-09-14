81207a9266a969de0fb1c883466bbd32bf6a66b4
seancrowe5 <sean.crowe@me.com>
2015-09-14 13:57:06 -0500

    added perry id

--- perry/revuup-process.php
+++ perry/revuup-process.php
@@ -21,7 +21,7 @@ ParseClient::initialize('KjqhJkgvtVSsPA9SVHxq1Euad73fWhLWfVS4LNxO', '9V1I071QAS4
         $name = $_POST["name"];
         $phone = $_POST["cell-phone"];
         
-        $perryGymID = '';
+        $perryGymID = 'CoFfrJ9Uwz';
         
         //we just need the user phone and gym object
         //let's now associate the user with REVUUP as 'paying' customers
