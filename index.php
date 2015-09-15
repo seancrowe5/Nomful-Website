cba2b618d45b5613278790f8fea4ff9075fec22f
seancrowe5 <sean.crowe@me.com>
2015-09-15 08:28:40 -0500

    [deploy: production]

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 
     
   <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> 
   <![endif]-->
 
 </head>
