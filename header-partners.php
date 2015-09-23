53f35e9c0b40d750051b277fc5328f916149cf53
Thomas Gorczynski <me@thomasgorczynski.com>
2015-09-23 12:41:15 -0500

    partners page

---
+++ header-partners.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8" /> 
+  <title>nomful | be nomful about food</title>
+  <!--<meta name="keywords" content=""/>
+  <meta name="description" content="nomful | be nomful about food" />
+  <meta property="og:type" content="website" /> 
+  <meta property="og:title" content="" /> 
+  <meta property="og:image" content="" /> 
+  <meta property="og:description" content="" /> 
+  <meta property="og:url" content="https://nomful.com">
+  <meta name="author" content="nomful" /> 
+  <meta name="robots" content="index, follow, noydir" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="google-site-verification" content="s-PDv36DeQ8GbPWkvISgveTSpQBsb7BQSMJTfiK_7Ps" />
+  <link rel="icon" 
+    type="image/png" 
+    href="">-->
+  <meta name="google-site-verification" content="s-PDv36DeQ8GbPWkvISgveTSpQBsb7BQSMJTfiK_7Ps" />
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css1/normalize.css">
+  <link rel="stylesheet" href="css1/skeleton.css">
+  <link rel="stylesheet" href="css1/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel='stylesheet prefetch' href='https://daneden.github.io/animate.css/animate.min.css'>
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="/../images/favicon.png">
+
+  <!-- SCRIPTS
+  ================================================== -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/wow/0.1.12/wow.min.js'></script>
+
+  <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-62272031-2', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  
+</head>
+<body>
+
+<div class="mobile-nav"> <!-- Hidden if the screen is larger than XXX px -->
+  <a href="http://nomful.com"><img class="logo-mobile" src="img1/nomful-partners.png"></a>
+  <div class="button_container" id="toggle">
+    <span class="top"></span>
+    <span class="middle"></span>
+    <span class="bottom"></span>
+  </div>
+  <div class="overlay" id="overlay">
+    <nav class="overlay-menu">
+      <ul>
+        <li ><a href="#">Home</a></li>
+        <li ><a href="https://nomful.com/join/">Sign-up</a></li>
+        <!--<li><a href="#">About</a></li>-->
+        <li><a href="/partners.php">Partners</a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
+
+<header>
+  <div class="header">
+    <a href="http://nomful.com"><img class="logo" src="img1/nomful-partners.png"></a>
+    <nav class="reg-menu">
+      <ul>
+        <!--<li><a href="#">About</a></li> -->
+        <li><a href="/partners.php">Partners</a></li>
+        <li><a href="/join/" class="sign-up capitalize">Sign up</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
