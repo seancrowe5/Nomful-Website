f17cb57bd30a6bb31a92a8b20db96c6e0cf3e56a
seancrowe5 <sean.crowe@me.com>
2015-11-12 21:16:26 -0600

    sean slack

---
+++ curltest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+$payload = array("text" => "hello!!!!",);                                                                    
+$data_string = json_encode($payload);                                                                                   
+                                                                                                                     
+$ch = curl_init('https://hooks.slack.com/services/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');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',                                                                                
+    'Content-Length: ' . strlen($data_string))                                                                       
+);                                                                                                                   
+                                                                                                                     
+$result = curl_exec($ch);
+
+
+?>
