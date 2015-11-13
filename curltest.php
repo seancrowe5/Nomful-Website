5fd0e9ec1c6105d03daf57f212de1a94d4f99557
seancrowe5 <sean.crowe@me.com>
2015-11-12 21:30:16 -0600

    name

--- curltest.php
+++ curltest.php
@@ -1,10 +1,11 @@
 <?php
 
 
-
+$name = "sean";
 $phoneNumber = "3306714458";
 
-$payload = array("text" => "New person is signing up!!: $phoneNumber");                                                                    
+$payload = array("text" => "Name: $name signed up with phone: $phoneNumber",
+                "username" => );                                                                    
 $data_string = json_encode($payload);                                                                                   
                                                                                                                      
 $ch = curl_init('https://hooks.slack.com/services/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');                                                                      
