3f36a474754e8b126f9d91c7eef37bf4ae138c31
seancrowe5 <sean.crowe@me.com>
2015-11-12 21:22:58 -0600

    k I'm done

--- curltest.php
+++ curltest.php
@@ -4,7 +4,7 @@
 
 $phoneNumber = "3306714458";
 
-$payload = array("text" => "New person is signing up!!: .'$phoneNumber'.");                                                                    
+$payload = array("text" => "New person is signing up!!: $phoneNumber");                                                                    
 $data_string = json_encode($payload);                                                                                   
                                                                                                                      
 $ch = curl_init('https://hooks.slack.com/services/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');                                                                      
