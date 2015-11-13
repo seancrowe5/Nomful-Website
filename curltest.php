f812989a4365d34c63c0950739c794fd02d0dc35
seancrowe5 <sean.crowe@me.com>
2015-11-12 21:46:38 -0600

    f

--- curltest.php
+++ curltest.php
@@ -3,26 +3,20 @@
 
 $name = "sean";
 $phoneNumber = "3306714458";
-//sms:3306714458
-$payload = array(
+sms:3306714458
 
-   "attachments":[
-      {
-         "fallback":"New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
-         "pretext":"New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
-         "color":"#D00000",
-         "fields":[
-            {
-               "title":"Notes",
-               "value":"This is much easier than I thought it would be.",
-               "short":false
-            }
-         ]
-      }
-   ]
+$fields = array("title" => "Notes",
+               "value" => "This is much easier than I thought it would be.",
+               "short" => false);
 
+$attachments = array(   "fallback" => "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                        "pretext" => "New open task [Urgent]: <http://url_to_task|Test out Slack message attachments>",
+                        "color" => "#D00000",
+                        "fields" => $fields);
 
-);                                                                    
+$payload = array("text" => "Name: $name signed up with phone: $phoneNumber",
+                "attachments" => $attachments); 
+
 $data_string = json_encode($payload);                                                                                   
                                                                                                                      
 $ch = curl_init('https://hooks.slack.com/services/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');                                                                      
