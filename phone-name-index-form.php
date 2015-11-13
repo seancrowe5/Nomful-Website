e117e64f60dbde6b2b5c458e1564a4ba7270c3a4
Thomas Gorczynski <me@thomasgorczynski.com>
2015-11-12 23:15:11 -0600

    textx

--- phone-name-index-form.php
+++ phone-name-index-form.php
@@ -34,7 +34,7 @@
         if (curl_exec($ch) || mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Have an awesome day :)";
+            echo "Awesome! You’ve taken the first step towards a healthier lifestyle. One of our experts will be in touch soon to match you with the perfect coach";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
