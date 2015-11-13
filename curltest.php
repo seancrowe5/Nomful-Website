40908ae7194aad316ddba0301cb0a642d38a7222
seancrowe5 <sean.crowe@me.com>
2015-11-12 21:31:00 -0600

    asdf

--- curltest.php
+++ curltest.php
@@ -4,8 +4,7 @@
 $name = "sean";
 $phoneNumber = "3306714458";
 
-$payload = array("text" => "Name: $name signed up with phone: $phoneNumber",
-                "username" => );                                                                    
+$payload = array("text" => "Name: $name signed up with phone: $phoneNumber");                                                                    
 $data_string = json_encode($payload);                                                                                   
                                                                                                                      
 $ch = curl_init('https://hooks.slack.com/services/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');                                                                      
