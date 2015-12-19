db09c9e3c64644fde990792ddd4bd8e1d3d0cea4
Thomas Gorczynski <me@thomasgorczynski.com>
2015-12-19 09:39:34 -0600

    just playing around

--- download/index.php
+++ download/index.php
@@ -1,55 +1,256 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
     <head>
-        <meta charset="UTF-8">
-        <script type="text/javascript">
-(function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-v1.8.3.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIdentity track validateCode".split(" "), 0);
-branch.init('key_test_mecNdlfYel5IvYJXZtBVIhaotynBT0K2');
 
-            function sendSMS(form) {
-                var phone = form.phone.value;
-                var linkData = {
-                    tags: [],
-                    channel: 'Flyer Landing Page',
-                    feature: 'TextMeTheApp',
-                    data: {}
-                };
-                var options = {};
-                var callback = function(err, result) {
-                    if (err) {
-                        alert("Sorry, something went wrong.");
-                    }
-                    else {
-                        alert("SMS sent!");
-                    
-                    }
-                };
-                branch.sendSMS(phone, linkData, options, callback);
-                form.phone.value = "";
-            }
-        </script>
-    </head>
-    <body>
-    	Send SMS
-        <form onsubmit="sendSMS(this); return false;">
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8" /> 
+  <title>nomful | be nomful about food</title>
+  <!--<meta name="keywords" content=""/>
+  <meta name="description" content="nomful | be nomful about food" />
+  <meta property="og:type" content="website" /> 
+  <meta property="og:title" content="" /> 
+  <meta property="og:image" content="" /> 
+  <meta property="og:description" content="" /> 
+  <meta property="og:url" content="https://nomful.com">
+  <meta name="author" content="nomful" /> 
+  <meta name="robots" content="index, follow, noydir" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="google-site-verification" content="s-PDv36DeQ8GbPWkvISgveTSpQBsb7BQSMJTfiK_7Ps" />
+  <link rel="icon" 
+    type="image/png" 
+    href="">-->
+  <meta name="google-site-verification" content="s-PDv36DeQ8GbPWkvISgveTSpQBsb7BQSMJTfiK_7Ps" />
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="/../css1/normalize.css">
+  <link rel="stylesheet" href="/../css1/skeleton.css">
+  <link rel="stylesheet" href="/../css1/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel='stylesheet prefetch' href='https://daneden.github.io/animate.css/animate.min.css'>
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="/../images/favicon.png">
+
+  <!-- SCRIPTS
+  ================================================== -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/wow/0.1.12/wow.min.js'></script>
+
+  <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  
+  <script src="//cdn.optimizely.com/js/3820750056.js"></script>
+  
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-62272031-2', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
+    <script type="text/javascript">
+      (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-v1.8.3.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIdentity track validateCode".split(" "), 0);
+      branch.init('key_test_mecNdlfYel5IvYJXZtBVIhaotynBT0K2');
+
+      function sendSMS(form) {
+        var phone = form.phone_number_a.value;
+        var linkData = {
+            tags: [],
+            channel: 'Flyer Landing Page',
+            feature: 'TextMeTheApp',
+            data: {}
+        };
+        var options = {};
+        var callback = function(err, result) {
+          if (err) {
+            alert("Sorry, something went wrong.");
+          }
+          else {
+            alert("SMS sent!");
+          }
+        };
+        branch.sendSMS(phone, linkData, options, callback);
+          form.phone_number_a.value = "";
+        }
+    </script>
+  
+</head>
+<body>
+
+
+  <header>
+    <div class="header">
+      <a href="http://nomful.com"><img class="logo" src="/../img1/nomful.png"></a>
+    </div>
+  </header>
+  
+  <section class="masthead-top">
+  <div class="container masthead-top-content">
+    <h1 id="top-headline">Your personal trainer for what you eat.</h1>
+    
+    <h4 id="subline">A nutrition coach in your pocket while you shop, cook, eat, and exercise.</h4>
+    
+    <script src="/js1/jquery.label_better.js"></script>
+    <script src="phone-index-form.js"></script>
+    <script src="phone-name-index-form.js"></script>
+    <script src="email-index-form.js"></script>
+    <div id="index-message-top" class="container-480"><div id="index-message"></div></div>
+    
+    <form id="ajax-phone-index" class="container-370 u-pull-left" onsubmit="sendSMS(this); return false;" novalidate>
+    <div class="row">
+      <input class="u-full-width label_better" data-new-placeholder="phone number" type="tel" placeholder="phone number" name="phone_number_a" id="phone_number_a">
+      <input type="text"  id="sp-website-r" name="sp-website-r" value=""  />
+      <input type="phone" id="sp-phone-r" name="sp-phone-r" value="" />
+      <input class="button-primary" type="submit" value="Find a coach">
+    </div>
+    </form>
+    
+</section>  
+  
+<section class="masthead masthead-explainer">
+  <div class="container masthead-explainer-content">
+    <h2 class="bold">Have confidence in what you eat</h2>
+    <!--<h4>There's a nutrition coach in your pocket while you shop, cook, eat, and exercise.</h4>-->
+    <div class="row">
+      <div class="explainer-step">
+        <span class="explainer-step-number">1</span>
+        <h4>Personalized for you</h4>
+        <p>Real coaching from the experts in habit-based nutrition. They get to know you, and together make a plan that works.</p>
+        <img src="/../img1/iphoneCall.jpg"/>
+      </div>
+      <div class="explainer-step">
+        <span class="explainer-step-number">2</span>
+        <h4>Pictures, not calories</h4>
+        <p>Instantly share meals with your coach and get feedback. We focus on building a healthy lifestyle, not counting calories.</p>
+        <img src="/../img1/marketingscreen-meal.jpg"/>
+      </div>
+      <div class="explainer-step">
+        <span class="explainer-step-number">3</span>
+        <h4>Daily support</h4>
+        <p>What's for dinner? What's whole wheat? You have your coach's attention everyday to answer questions and give feedback.</p>
+        <img src="/../img1/chat.jpg"/>
+      </div>
+    </div>
+  </div>
+</section>
+  
+  <section class="masthead masthead-join">
+  <div class="container masthead-join-content">
+    <h2 id="join-title">Forget fads and restrictions, build a lifestyle right for you</h2>
+    <div id="healthy-start">
+      <h4>21 Day Healthy Start</h4>
+      <h2>$49</h2>
+      <ul class="join-props">
+        <li>Initial phone assesment</li>
+        <li>Daily feedback and support</li>
+        <li>Quick meal sharing</li>
+        <li>Help with recipes, questions, etc.</li>
+      </ul>
+      <a href="https://itunes.apple.com/US/app/id991589420?mt=8#"><button class="button join-button">Get Started</button></a>
+    </div>
+    <div id="bootcamp">
+      <h4>12-Week Boot Camp</h4>
+      <h2>$199</h2>
+      <ul class="join-props">
+        <li>Complete Personalized Evaluation</li>
+        <li>Regular phone assesments</li>
+        <li>Daily accountability</li>
+        <li>Quick meal sharing</li>
+        <li>Weekly progress tracking</li>
+      </ul>
+      <a href="https://itunes.apple.com/US/app/id991589420?mt=8#"><button class="button-primary join-button">Get Started</button></a>
+    </div>
+    <h4 class="question-contact">Have a question? <a href="contact.php">Get in touch with us!</a></h4>
+  </div>
+</section>  
+
+<!--        <form onsubmit="sendSMS(this); return false;">
         	<input id="phone" name="phone" type="tel" placeholder="(650) 123-4567" />
         	<br/>
         	<input type="submit"/>
-        </form>
-    </body>
-</html>
+        </form>-->
+  
+<script src="/../js1/scale.fix.js"></script>
+<script src="/../js1/formatter.js"></script>
+  
+  <script>
+    new Formatter(document.getElementById('phone_number_a'), {
+    'pattern': '({{999}}) {{999}} - {{9999}}',
+    'persistent': false
+  });
+  </script>
 
 <?php
-    require "vendor/autoload.php";
+    /*require "vendor/autoload.php";
     $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
     $result = $MailChimp->call('lists/subscribe', array(
-                    'id'                => '297cbd7828',
-                    'email'             => array('email'=>'sean@nomful.com'),
-                    'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
-                    'double_optin'      => false,
-                    'update_existing'   => true,
-                    'replace_interests' => false,
-                    'send_welcome'      => false,
-                ));
-    print_r($result);
+        'id'                => '297cbd7828',
+        'email'             => array('email'=>'sean@nomful.com'),
+        'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
+        'double_optin'      => false,
+        'update_existing'   => true,
+        'replace_interests' => false,
+        'send_welcome'      => false,
+    ));
+    print_r($result);*/
 ?>
+  
+
+<footer>
+  <div class="top-footer">
+    <div class="learn-more">
+      <h6 class="hide-less-tablet uppercase">Learn more</h6>
+      <img class="footer-logo show-less-tablet" src="img1/nomful-white.png"/>
+      <ul class="learn-more-list list-left">
+        <li><a href="https://nomful.com/partners/">Partners</a></li>
+        <li><a href="https://nomful.com/contact.php">Contact</a></li>
+      </ul>
+      <ul class="learn-more-list list-right">
+        <li><a href="https://nomful.com/privacy.php">Privacy</a></li>
+        <li><a href="https://nomful.com/terms.php">Terms</a></li>
+      </ul>
+    </div>
+
+    <a href="https://itunes.apple.com/US/app/id991589420?mt=8#" target="_blank"><img class="download-appstore" src="/../img1/appstore-download.png" /></a>
+      
+    <div class="connect">
+      <h6 class="hide-less-tablet uppercase">Connect with us</h6>
+      <ul class="social-media-icons">
+        <li><a href="https://instagram.com/nomful/" target="_blank"><img src="/../img1/icons/instagram.png"/></a></li>
+        <li><a href="https://www.facebook.com/benomful/" target="_blank"><img src="/../img1/icons/facebook.png"/></a></li>
+        <li><a href="https://www.linkedin.com/company/nomful" target="_blank"><img src="/../img1/icons/linkedin.png"/></a></li>
+        <li><a href="https://mobile.twitter.com/benomful/" target="_blank"><img src="/../img1/icons/twitter.png"/></a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="bottom-footer">
+    <div class="container">
+      <h6 class="made-with">Made with <i class="fa fa-heart"></i> in Chicago</h6>
+      <p class="copyright">&copy; 2015 Nomful, Inc. - All Rights Reserved</p>
+    </div>
+  </div>
+</footer> 
+    
+<script src="/../js1/index.js"></script> 
+
+</body>
+</html>
+
+
