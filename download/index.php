fa68c4bdfad2d6b4518f8248e70703487d6445e3
Thomas Gorczynski <me@thomasgorczynski.com>
2015-12-19 17:10:39 -0600

    getting rid

--- download/index.php
+++ download/index.php
@@ -114,20 +114,7 @@
       </form>
     </div>
     
-    <img id="download-phone-right" src="/../img1/marketingscreen-meal.jpg"/>
-        
-    <?php 
-       /* //call parse cloud
-        require 'vendor/autoload.php';
-        use Parse\ParseClient;
-        use Parse\ParseCloud;
-        ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5');
-
-        //run cloud code and pass it an email
-        $email = ''; //email here!
-        ParseCloud::run("sendInstallEmail", array("toEmail" => "sean.crowe@me.com"));*/
-    ?>
-    
+    <img id="download-phone-right" src="/../img1/marketingscreen-meal.jpg"/>    
     
 </section>  
   
@@ -157,12 +144,6 @@
     </div>
   </div>
 </section>
-
-<!--        <form onsubmit="sendSMS(this); return false;">
-        	<input id="phone" name="phone" type="tel" placeholder="(650) 123-4567" />
-        	<br/>
-        	<input type="submit"/>
-        </form>-->
   
 <script src="/../js1/scale.fix.js"></script>
 <script src="/../js1/formatter.js"></script>
@@ -173,22 +154,7 @@
     'persistent': false
   });
   </script>
-
-<?php
-    /*require "vendor/autoload.php";
-    $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
-    $result = $MailChimp->call('lists/subscribe', array(
-        'id'                => '297cbd7828',
-        'email'             => array('email'=>'sean@nomful.com'),
-        'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
-        'double_optin'      => false,
-        'update_existing'   => true,
-        'replace_interests' => false,
-        'send_welcome'      => false,
-    ));
-    print_r($result);*/
-?>
-  
+        
 
 <footer>
   <div class="top-footer">
