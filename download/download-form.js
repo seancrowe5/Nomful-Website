ce58bd003becca117862157e33c6b1d3ee4c449f
Thomas Gorczynski <me@thomasgorczynski.com>
2015-12-19 18:32:26 -0600

    udpates

--- download/download-form.js
+++ download/download-form.js
@@ -38,6 +38,8 @@ $(function() {
 			url: $(form).attr('action'),
 			data: formData
 		})
+        
+        function(sendSMS('input#phone_number_a'));
       
 		.done(function(response) {
 			// Make sure that the formMessages div has the 'success' class.
@@ -73,4 +75,28 @@ $(function() {
 });
 
 
+(function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-v1.8.3.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIden...(line truncated)...
+      branch.init('key_test_mecNdlfYel5IvYJXZtBVIhaotynBT0K2'); //key_live_kbmLoonXji6GF6P8YutPaifnztbFJ1QG 
+
+
+function sendSMS(form) {
+  var phone = form.phone_number_a.value;
+  var linkData = {
+      tags: [],
+      channel: 'Flyer Landing Page',
+      feature: 'TextMeTheApp',
+      data: {}
+  };
+  var options = {};
+  var callback = function(err, result) {
+    if (err) {
+      alert("Sorry, something went wrong.");
+    }
+    else {
+      alert("SMS sent!");
+    }
+  };
+  branch.sendSMS(phone, linkData, options, callback);
+    form.phone_number_a.value = "";
+  }
 
