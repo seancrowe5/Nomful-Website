2af5cf9af0527f8a78ecc6557f14f2669ae2fc8f
seancrowe5 <sean.crowe@me.com>
2015-12-20 12:38:40 -0600

    yo

--- download/index.php
+++ download/index.php
@@ -54,7 +54,7 @@
   <![endif]--> 
         
 
-
+<!--comment back in
 <script> src="//cdn.optimizely.com/js/3820750056.js"></script>
         
 <script>  
@@ -67,7 +67,7 @@
       ga('send', 'pageview');
 </script>
 
-
+-->
         
 <script>
     (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-v1.8.3.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIdentity track validateCode".split(" "), 0);
