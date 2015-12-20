d8d5d79f45ca5d5a706940020bb3487d70509454
seancrowe5 <sean.crowe@me.com>
2015-12-20 13:06:49 -0600

    [deploy: production]

--- download/index.php
+++ download/index.php
@@ -54,7 +54,6 @@
   <![endif]--> 
         
 
-<!--comment back in
 <script> src="//cdn.optimizely.com/js/3820750056.js"></script>
         
 <script>  
@@ -67,11 +66,10 @@
       ga('send', 'pageview');
 </script>
 
--->
         
 <script>
     (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-v1.8.3.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setIdentity track validateCode".split(" "), 0);
-branch.init('key_test_mecNdlfYel5IvYJXZtBVIhaotynBT0K2'); 
+branch.init('key_live_kbmLoonXji6GF6P8YutPaifnztbFJ1QG'); 
 
           function sendSMS(form) {
               var phone = form.phone_number_a.value;
