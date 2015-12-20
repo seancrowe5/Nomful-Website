c8d945cebbb11ae7fef5b401fd09efed808ee487
Thomas Gorczynski <me@thomasgorczynski.com>
2015-12-19 19:33:04 -0600

    all it works except the important stuff lol

--- download/download-form.php
+++ download/download-form.php
@@ -31,27 +31,28 @@
         );                                                                                                                   
 */
       
-/*      //call parse cloud
-        require 'vendor/autoload.php';
-        use Parse\ParseClient;
-        use Parse\ParseCloud;
-        ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5');
-
-        //run cloud code and pass it an email
-        //$email = ''; //email here!  ---- ALREADY GOT THIS VARIABLE ABOVE
-        ParseCloud::run("sendInstallEmail", array("toEmail" => "sean.crowe@me.com"));*/
+//      //call parse cloud
+//        require 'vendor/autoload.php';
+//        use Parse\ParseClient;
+//        use Parse\ParseCloud;
+//        ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5');
+//
+//        //run cloud code and pass it an email
+//        //$email = ''; //email here!  ---- ALREADY GOT THIS VARIABLE ABOVE
+//        ParseCloud::run("sendInstallEmail", array("toEmail" => "sean.crowe@me.com"));
+//      
+//        $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
+//        $result = $MailChimp->call('lists/subscribe', array(
+//            'id'                => '297cbd7828',
+//            'email'             => array('email'=>'sean@nomful.com'),
+//            'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
+//            'double_optin'      => false,
+//            'update_existing'   => true,
+//            'replace_interests' => false,
+//            'send_welcome'      => false,
+//        ));
       
-        $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
-        $result = $MailChimp->call('lists/subscribe', array(
-            'id'                => '297cbd7828',
-            'email'             => array('email'=>'sean@nomful.com'),
-            'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
-            'double_optin'      => false,
-            'update_existing'   => true,
-            'replace_interests' => false,
-            'send_welcome'      => false,
-        ));
-        //print_r($result);
+//        print_r($result);
 
         // Send the email.
         if (mail($recipient, $subject, $email_content)) {
