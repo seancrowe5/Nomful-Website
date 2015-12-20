8248a712e249c216a69070175a097551e30e6ad7
Thomas Gorczynski <me@thomasgorczynski.com>
2015-12-19 19:59:40 -0600

    mailchimp

--- download/download-form.php
+++ download/download-form.php
@@ -39,16 +39,16 @@ ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJy
         //run cloud code and pass it an email
         ParseCloud::run("sendInstallEmail", array("toEmail" => $email ));
       
-//        $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
-//        $result = $MailChimp->call('lists/subscribe', array(
-//            'id'                => '297cbd7828',
-//            'email'             => array('email'=>'sean@nomful.com'),
-//            'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
-//            'double_optin'      => false,
-//            'update_existing'   => true,
-//            'replace_interests' => false,
-//            'send_welcome'      => false,
-//        ));
+        $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
+        $result = $MailChimp->call('lists/subscribe', array(
+            'id'                => '297cbd7828',
+            'email'             => array('email'=>'sean@nomful.com'),
+            'merge_vars'        => array('FNAME'=>'Sean', 'LNAME'=>'Crowe', 'SOURCE' => 'Nomful Download Landing'),
+            'double_optin'      => false,
+            'update_existing'   => true,
+            'replace_interests' => false,
+            'send_welcome'      => false,
+        ));
       
 //        print_r($result);
 
