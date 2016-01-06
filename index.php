70e6ed2d3d57814e95f4180be3051b69b01e0a8e
Thomas Gorczynski <me@thomasgorczynski.com>
2016-01-06 10:33:24 -0600

    lots of good changes

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
   <div class="container masthead-testimonial-content">
     
     <div class="testimonial-box">
-      <img class="testimonial-profile" src="img1/Steph.jpg">
+      <img class="testimonial-profile" src="img1/Jody.jpg">
       <div class="testimonial-stars-row">
         <img class="testimonial-stars" src="img1/star.png">
         <img class="testimonial-stars" src="img1/star.png">
@@ -44,8 +44,8 @@
         <img class="testimonial-stars" src="img1/star.png">
         <img class="testimonial-stars" src="img1/star.png">
       </div>
-      <h4>"The Nomful program allowed me to break my bad habits!"</h4>
-      <h5>-Ashley, 24</h5>
+      <h5>"The Nomful program allowed me to break my bad habits!"</h5>
+      <h6>-Jody, XX</h6>
     </div>
     
   </div>
@@ -58,7 +58,7 @@
     
     <div class="eatingRight-point">
       <div class="point-box tables">
-        <div class="cells"><img class="point-box-img" src="img1/Ashley.png" /></div>
+        <div class="cells"><img class="point-box-img" src="img1/Ashley.jpg" /></div>
         <div class="cells"><p>I just have to track food with photos, not calories. And then my coach, Emily, gives me a personalized plan just for me.</p></div>
       </div>
     <p class="point-box-person">Ashley, 27</p>
@@ -67,10 +67,10 @@
     
     <div class="eatingRight-point">
       <div class="point-box tables" id="point-box-green">
-        <div class="cells"><img class="point-box-img" src="img1/emily.png" /></div>
+        <div class="cells"><img class="point-box-img" src="img1/Steph.jpg" /></div>
         <div class="cells"><p>With photos, I can see  what you're eating, when you're eating it, and how big your portions are. All I need to help re-align your diet!</p></div>
       </div>
-    <p class="point-box-person">Coach Emily</p>
+    <p class="point-box-person">Coach Stephanie</p>
     </div>
     
     
@@ -89,13 +89,13 @@
     </div>
     
     <div class="coachIntro-box">
-      <img src="img1/emily.png">
+      <img src="img1/Emily.jpg">
       <h4>Emily G.</h4>
       <p>I come from a close-knit Irish family - youngest of seven kids! My passion for nutrition began when I was diagnosed with type 1 diabetes. I decided then that I wanted to help people like me, which is why I’m becoming a certified diabetes educator. I enjoy yoga, cooking, reading nutrition blogs, and binging on reality TV.</p>
     </div>
     
     <div class="coachIntro-box">
-      <img src="img1/amy.png">
+      <img src="img1/Amy.jpg">
       <h4>Amy D.</h4>
       <p>Born and raised in New Orleans, I've always had a passion for food. When I'm not helping people reach their health goals, I'm spending too much time browsing the aisles of Whole Foods, recipe testing for my blog, The Balanced Dietitian, trying new meals at local restaurants, or walking through beautiful Audubon Park!</p>
     </div>
@@ -104,38 +104,6 @@
     
   </div>
 </section>
-  
-  <section class="masthead masthead-join">
-  <div class="container masthead-join-content">
-    <h2 id="join-title">Forget fads and restrictions, build a lifestyle right for you</h2>
-    <div id="healthy-start">
-      <h4>21 Day Healthy Start</h4>
-      <h2>$49</h2>
-      <ul class="join-props">
-        <li>Initial phone assesment</li>
-        <li>Daily feedback and support</li>
-        <li>Quick meal sharing</li>
-        <li>Help with recipes, questions, etc.</li>
-      </ul>
-      <a href="https://itunes.apple.com/US/app/id991589420?mt=8#"><button class="button join-button">Get Started</button></a>
-    </div>
-    <div id="bootcamp">
-      <h4>12-Week Boot Camp</h4>
-      <h2>$199</h2>
-      <ul class="join-props">
-        <li>Complete Personalized Evaluation</li>
-        <li>Regular phone assesments</li>
-        <li>Daily accountability</li>
-        <li>Quick meal sharing</li>
-        <li>Weekly progress tracking</li>
-      </ul>
-      <a href="https://itunes.apple.com/US/app/id991589420?mt=8#"><button class="button-primary join-button">Get Started</button></a>
-    </div>
-    <h4 class="question-contact">Have a question? <a href="contact.php">Get in touch with us!</a></h4>
-  </div>
-</section>  
- 
-  
   
 <div class="subscribe-me">
   <h2>Stay in touch!</h2>
