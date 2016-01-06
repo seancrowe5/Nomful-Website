7a5d33b977cdfd4551a17e49bc39e41daa917c75
seancrowe5 <sean.crowe@me.com>
2016-01-06 09:59:46 -0600

    removed parse from download

--- download/download-form.php
+++ download/download-form.php
@@ -1,9 +1,6 @@
 <?php
 //call parse cloud
 require 'vendor/autoload.php';
-use Parse\ParseClient;
-use Parse\ParseCloud;
-ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5');
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -36,18 +33,15 @@ ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJy
             'Content-Length: ' . strlen($data_string))                                                                       
         );                                                                                                                   
 
-        //run cloud code and pass it an email
-        //ParseCloud::run("sendInstallEmail", array("toEmail" => $email ));
-      
         $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
         $result = $MailChimp->call('lists/subscribe', array(
             'id'                => '297cbd7828',
             'email'             => array('email'=>$email),
-            'merge_vars'        => array('PLAN' => 'Trial'),
+            'merge_vars'        => array('TYPE' => 'Trial'),
             'double_optin'      => false,
             'update_existing'   => true,
             'replace_interests' => false,
-            'send_welcome'      => false,
+            'send_welcome'      => true,
         ));
       
 //        print_r($result);
