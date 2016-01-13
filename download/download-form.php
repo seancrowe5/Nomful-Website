c18ef8467b3899337f60bc99ed1dcfb55ac088dc
seancrowe5 <sean.crowe@me.com>
2016-01-13 13:34:50 -0600

    mail chimp to cloud

--- download/download-form.php
+++ download/download-form.php
@@ -1,6 +1,10 @@
 <?php
 //call parse cloud
 require 'vendor/autoload.php';
+use Parse\ParseClient;
+use Parse\ParseCloud;
+ParseClient::initialize('EcHepDGBmNvZhRx8D1vMFLzMPgqAXqfIjpiIJuIe', 'cyksn8TZdJyJVIeM0RbTMkGmHlped7dMhoksgrxm', 'fk2DwxckV3Pe4fKGRs6LH1YaZkslDTu86TS6Ouv5'); //dev
+
 
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -31,16 +35,9 @@ require 'vendor/autoload.php';
             'Content-Length: ' . strlen($data_string))                                                                       
         );                                                                                                                   
 
-        $MailChimp = new \Drewm\MailChimp('458779a92e35ef155beeb58b445fd2ee-us10');
-        $result = $MailChimp->call('lists/subscribe', array(
-            'id'                => '297cbd7828',
-            'email'             => array('email'=>$email),
-            'merge_vars'        => array('TYPE' => 'Trial'),
-            'double_optin'      => false,
-            'update_existing'   => true,
-            'replace_interests' => false,
-            'send_welcome'      => true,
-        ));
+       
+        //ADD USER TO MAILCHIMP
+        ParseCloud::run("addUserToMailchimpList", array("toEmail" => $email));
       
 //        print_r($result);
 
