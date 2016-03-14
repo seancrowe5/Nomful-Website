7e20db23178ef78b8b452ae17e4d77490451a877
seancrowe5 <sean.crowe@me.com>
2016-03-14 13:13:18 -0500

    key

--- verizon/config.php
+++ verizon/config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = array(
-  "secret_key"      => "sk_test_BQokikJOvBiI2HlWgH4olfQ2",
-  "publishable_key" => "pk_test_6pRNASCoBOKtIshFeQd4XMUh"
+  "secret_key"      => "sk_live_SYYhkBbbtoAt503sMgJpGKNY",
+  "publishable_key" => "pk_live_a7plqC3BMb74TAFBiefltyaf"
 );
 
 \Stripe\Stripe::setApiKey($stripe['sk_live_SYYhkBbbtoAt503sMgJpGKNY']);
