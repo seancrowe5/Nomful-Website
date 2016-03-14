2c4eb0b58b6e7dc9cf894e4b872354789e4c3375
seancrowe5 <sean.crowe@me.com>
2016-03-14 13:09:26 -0500

    api

--- verizon/index.php
+++ verizon/index.php
@@ -130,7 +130,7 @@ require_once('./config.php');
 <form action="charge.php" method="post">
   <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
           data-key="<?php echo $stripe['publishable_key']; ?>"
-          data-description="Access for a year"
+          data-description="Nomful 12-week Bootcamp"
           data-amount="19900"
           data-locale="auto"></script>
 </form>
