eae38daff35c7d5478c3f04cfb74e669daaf6374
seancrowe5 <sean.crowe@me.com>
2016-03-14 13:02:13 -0500

    amount

--- verizon/index.php
+++ verizon/index.php
@@ -131,7 +131,7 @@ require_once('./config.php');
   <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
           data-key="<?php echo $stripe['publishable_key']; ?>"
           data-description="Access for a year"
-          data-amount="5000"
+          data-amount="19900"
           data-locale="auto"></script>
 </form>
 
