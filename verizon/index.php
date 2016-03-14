85dae7abde198c4c3e040b6d4875ca9e45f54ecb
Thomas Gorczynski <me@thomasgorczynski.com>
2016-03-14 14:01:09 -0500

    done

--- verizon/index.php
+++ verizon/index.php
@@ -142,7 +142,7 @@ $(function() {
         <li>Weekly progress tracking</li>
         <strong><center>BONUS</center></strong>
         <li>Meal Patter Guide ($75 value)</li>
-        <li>Extra phone consultation ($50 value)</li>
+        <li>Extra phone consultation ($50 value) </li>
       </ul>
       <form action="charge.php" method="post">
         <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
